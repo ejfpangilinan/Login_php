a6c1251aa2d20c4f2e5b7dd866929a6ee982c4ab
Erl Jhon F. Pangilinan <efpangilinan1@up.edu.ph>
2021-07-30 04:53:49 +0800

    online database integration

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
     // to establish connection to database
     
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'login');
+    define('DB_SERVER', 'remotemysql.com');
+    define('DB_USERNAME', 'O1DmAGoPkZ');
+    define('DB_PASSWORD', 'by4y6xaQwl');
+    define('DB_NAME', 'O1DmAGoPkZ');
      
 
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
